96726207fae2cb2f62f79bcd85a91b23c55c41c2
NoahTheLegend <20jeka03@gmail.com>
2024-05-06 23:42:56 +0300

    work on hover effect for buttons

--- core/ui/preset.py
+++ core/ui/preset.py
@@ -1,3 +1,6 @@
 class default_preset:
     def getHoverColor(self) -> str:
         return "d8eaf9"
+    
+    def getBackgroundColor(self) -> str:
+        return "f0f0f0"
--- core/ui/ui_module.py
+++ core/ui/ui_module.py
@@ -1,4 +1,7 @@
+# Collection of custom classes for different solutions
+
 from utils.vec import vec
+from PyQt6 import QtWidgets
 
 class ui_module:
     def __init__(self):
@@ -10,3 +13,17 @@ class ui_module:
 
     def set_pos(self, vec):
         self.pos = vec
+
+class button(QtWidgets.QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.stylesheets = {}
+
+    def setHoverStyles(self, stylesheets):
+        self.stylesheets = stylesheets
+        
+    def enterEvent(self, event):
+        self.setStyleSheet(self.stylesheets['enter'])
+
+    def leaveEvent(self, event):
+        self.setStyleSheet(self.stylesheets['leave'])
