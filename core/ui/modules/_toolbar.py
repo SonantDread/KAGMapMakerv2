a2edf70548fb98acd19611e1622263806fe6c567
SonantDread <SonantDread@gmail.com>
2024-05-12 13:05:46 -0700

    bug fix

--- core/ui/modules/_toolbar.py
+++ core/ui/modules/_toolbar.py
@@ -47,56 +47,69 @@ class Toolbar(QToolBar):
         self.initUI()
 
     def initUI(self):
-        # File menu
-        file_menu = QMenu("File", self)
-        new_action = QAction("New", self)
-        save_action = QAction("Save", self)
-        save_as_action = QAction("Save As", self)
-        load_action = QAction("Load", self)
-        render_action = QAction("Render", self)
-        file_menu.addAction(new_action)
-        file_menu.addAction(save_action)
-        file_menu.addAction(save_as_action)
-        file_menu.addAction(load_action)
-        file_menu.addAction(render_action)
+        # file menu
+        _file_menu = QMenu("File", self)
+        _new_action = QAction("New", self)
+        _save_action = QAction("Save", self)
+        _save_as_action = QAction("Save As", self)
+        _load_action = QAction("Load", self)
+        _render_action = QAction("Render", self)
+        _file_menu.addAction(_new_action)
+        _file_menu.addAction(_save_action)
+        _file_menu.addAction(_save_as_action)
+        _file_menu.addAction(_load_action)
+        _file_menu.addAction(_render_action)
 
-        # Settings menu
-        settings_menu = QMenu("Settings", self)
-        example_checkbox = QCheckBox("Example Checkbox", self)
-        settings_widget_action = QWidgetAction(self)
-        settings_widget_action.setDefaultWidget(example_checkbox)
-        settings_menu.addAction(settings_widget_action)
+        # settings menu
+        _settings_menu = QMenu("Settings", self)
+        _example_checkbox = QCheckBox("Example Checkbox", self)
+        _settings_widget_action = QWidgetAction(self)
+        _settings_widget_action.setDefaultWidget(_example_checkbox)
+        _settings_menu.addAction(_settings_widget_action)
 
-        # View menu
-        view_menu = QMenu("View", self)
+        # view menu
+        _view_menu = QMenu("View", self)
         
-        # Create a submenu for buttons
-        buttons_submenu = QMenu("Buttons", self)
-        button1_action = QAction("Button 1", self)
-        button2_action = QAction("Button 2", self)
-        button3_action = QAction("Button 3", self)
-        buttons_submenu.addAction(button1_action)
-        buttons_submenu.addAction(button2_action)
-        buttons_submenu.addAction(button3_action)
+        # create a submenu for buttons
+        _buttons_submenu = QMenu("Buttons", self)
+        _button1_action = QAction("Button 1", self)
+        _button2_action = QAction("Button 2", self)
+        _button3_action = QAction("Button 3", self)
+        _buttons_submenu.addAction(_button1_action)
+        _buttons_submenu.addAction(_button2_action)
+        _buttons_submenu.addAction(_button3_action)
         
-        # Add the submenu to the "View" menu
-        view_menu.addMenu(buttons_submenu)
+        # add the submenu to the 'View' menu
+        _view_menu.addMenu(_buttons_submenu)
 
-        # Add menus to the toolbar
-        self.addAction(file_menu.menuAction())
-        self.addAction(settings_menu.menuAction())
-        self.addAction(view_menu.menuAction())
+        # connect actions to functions
+        _new_action.triggered.connect(self.new_triggered)
+        _save_action.triggered.connect(self.save_triggered)
+        _save_as_action.triggered.connect(self.save_as_triggered)
+        _load_action.triggered.connect(self.load_triggered)
+        _render_action.triggered.connect(self.render_triggered)
+        _example_checkbox.toggled.connect(self.example_checkbox_toggled)
+        _button1_action.triggered.connect(self.button1_triggered)
+        _button2_action.triggered.connect(self.button2_triggered)
+        _button3_action.triggered.connect(self.button3_triggered)
 
-        # Connect actions
-        new_action.triggered.connect(self.new_triggered)
-        save_action.triggered.connect(self.save_triggered)
-        save_as_action.triggered.connect(self.save_as_triggered)
-        load_action.triggered.connect(self.load_triggered)
-        render_action.triggered.connect(self.render_triggered)
-        example_checkbox.toggled.connect(self.example_checkbox_toggled)
-        button1_action.triggered.connect(self.button1_triggered)
-        button2_action.triggered.connect(self.button2_triggered)
-        button3_action.triggered.connect(self.button3_triggered)
+        # add File menu to toolbar
+        self.file_menu = QAction("File", self)
+        self.file_menu.triggered.connect(lambda: self._PopUp(_file_menu, self.file_menu))
+        self.addAction(self.file_menu)
+
+        # add Settings menu to toolbar
+        self.settings_menu = QAction("Settings", self)
+        self.settings_menu.triggered.connect(lambda: self._PopUp(_settings_menu, self.settings_menu))
+        self.addAction(self.settings_menu)
+
+        # add View menu to toolbar
+        self.view_menu = QAction("View", self)
+        self.view_menu.triggered.connect(lambda: self._PopUp(_view_menu, self.view_menu))
+        self.addAction(self.view_menu)
+
+    def _PopUp(self, tabtoopen, trigger):
+        return tabtoopen.popup(self.mapToGlobal(self.actionGeometry(trigger).bottomLeft()))
 
     def new_triggered(self):
         print("New triggered")
