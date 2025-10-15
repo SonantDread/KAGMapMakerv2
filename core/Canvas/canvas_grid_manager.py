0314f5204b30be9bc933da326b7fdbf8b2a3e5b6
SonantDread <SonantDread@gmail.com>
2025-10-15 05:23:31 -0700

    style: cleanup canvas_grid_manager.py

--- core/Canvas/canvas_grid_manager.py
+++ core/Canvas/canvas_grid_manager.py
@@ -1,8 +1,14 @@
+"""
+Manages the grid setting for the canvas.
+"""
 from PyQt6.QtWidgets import QGraphicsItemGroup
 from PyQt6.QtGui import QPen
 from PyQt6.QtCore import Qt
 
 class GridManager:
+    """
+    Sets up and handles the visibility of the grid.
+    """
     def __init__(self, canvas) -> None:
         self.canvas = canvas
 
@@ -17,9 +23,10 @@ class GridManager:
         Builds or rebuilds the tile grid on the canvas by creating grid lines.
         Clears any existing grid lines before drawing new ones.
         """
+        canvas = self.canvas
         self.grid_group = QGraphicsItemGroup()
-        self.canvas.canvas.addItem(self.grid_group)
-        is_visible = self.canvas.communicator.settings.get("tile grid visible", False)
+        canvas.canvas.addItem(self.grid_group)
+        is_visible = canvas.communicator.settings.get("tile grid visible", False)
         self.grid_group.setVisible(is_visible)
 
         pen = QPen(Qt.GlobalColor.black)
@@ -27,28 +34,21 @@ class GridManager:
         # prevent grid lines being different sizes
         pen.setCosmetic(True)
 
-        width = self.canvas.map_size.x * self.canvas.grid_spacing
-        height = self.canvas.map_size.y * self.canvas.grid_spacing
+        width, height = canvas.map_size * canvas.grid_spacing
 
         # vertical lines
-        for x in range(0, width + 1, self.canvas.grid_spacing):
-            line = self.canvas.canvas.addLine(x, 0, x, height, pen)
+        for x in range(0, width + 1, canvas.grid_spacing):
+            line = canvas.canvas.addLine(x, 0, x, height, pen)
             self.grid_group.addToGroup(line)
 
         # create horizontal grid lines
-        for y in range(0, height + 1, self.canvas.grid_spacing):
-            line = self.canvas.canvas.addLine(0, y, width, y, pen)
+        for y in range(0, height + 1, canvas.grid_spacing):
+            line = canvas.canvas.addLine(0, y, width, y, pen)
             self.grid_group.addToGroup(line)
 
     def set_grid_visible(self, show: bool = None) -> None:
         """
         Toggles the visibility of the grid on the canvas.
-
-        Args:
-            show (bool): Whether to show or hide the grid, or toggle if not specified.
-
-        Returns:
-            None
         """
         is_visible: bool = self.grid_group.isVisible()
 
