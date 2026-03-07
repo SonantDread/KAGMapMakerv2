f32f5e3bf1d2b5fff9c7d569cb8db6d01e51cdd7
SonantDread <SonantDread@gmail.com>
2026-03-06 17:52:47 -0800

    chore: add GPL license header

--- base/renderer.py
+++ base/renderer.py
@@ -1,3 +1,20 @@
+# KAGMapMakerV2 - An unofficial map maker for King Arthur's Gold.
+# Copyright (C) 2026 SonantDread
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 """
 Handles the rendering of objects for the canvas class.
 This class is responsible for all direct manipulation of the QGraphicsScene
--- utils/config_handler.py
+++ utils/config_handler.py
@@ -1,3 +1,20 @@
+# KAGMapMakerV2 - An unofficial map maker for King Arthur's Gold.
+# Copyright (C) 2026 SonantDread
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 """
 Handles configuration loading and custom item management for the application.
 """
